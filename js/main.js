74da13272876d21c167473a7801419af493f01d0
Raman Jee <ramanjee0806@gmail.com>
2023-08-17 16:00:53 +0530

    changed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 console.log("Raman is live!");
+let previousValue1 = "";
+let previousValue2 = "";
 
 function generate() {
     let first = document.getElementById('first-name').value;
     let write = document.getElementById('generate');
     let last = document.getElementById('last-name').value;
 
+    let currentValue1 = first;
+    let currentValue2 = last;
+    // let currentValue = first.value;
     if (first == '' || last == '') {
         alert("You ave missed any box...");
-    } else if (first.toLowerCase().includes("vikalp") && last.toLowerCase().includes("harshita")) {
-        write.value = `100%`;
+    } else if (currentValue1 == previousValue1 || currentValue2 == previousValue2) {
+        alert("You haven't changed the value.");
     } else if (first.toLowerCase().includes("vikalp") && !last.toLowerCase().includes("harshita")) {
         write.value = `0%`;
     } else if (!first.toLowerCase().includes("vikalp") && last.toLowerCase().includes("harshita")) {
@@ -20,5 +25,30 @@ function generate() {
         }, 100);
         let random = Math.floor(Math.random() * 100);
         write.value = `${random}%`;
+        previousValue1 = currentValue1;
+        previousValue2 = currentValue2;
+        console.log(previousValue1, previousValue2);
     }
 }
+// function generate() {
+//     let first = document.getElementById('first-name').value;
+//     let write = document.getElementById('generate');
+//     let last = document.getElementById('last-name').value;
+
+//     if (first == '' || last == '') {
+//         alert("You ave missed any box...");
+//     } else if (first.toLowerCase().includes("vikalp") && last.toLowerCase().includes("harshita")) {
+//         write.value = `100%`;
+//     } else if (first.toLowerCase().includes("vikalp") && !last.toLowerCase().includes("harshita")) {
+//         write.value = `0%`;
+//     } else if (!first.toLowerCase().includes("vikalp") && last.toLowerCase().includes("harshita")) {
+//         write.value = `0%`;
+//     } else {
+//         click.style.transform = 'translate(10px, 10px)'
+//         setTimeout(() => {
+//             click.style.transform = 'none'
+//         }, 100);
+//         let random = Math.floor(Math.random() * 100);
+//         write.value = `${random}%`;
+//     }
+// }
